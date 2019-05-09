249bdd240c904c13d89f696ef6ffdfec1c861b2b
Andry85 <shapovala@ukr.net>
2019-05-09 10:56:38 +0300

    added text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,7 +7,7 @@ class About extends Component {
     return (
       <section className="about">
           <h2 className="about__subtitle">- About me -</h2>
-          <p className="about__text">I’m an web designer and front-end developer from Vinnytsia, Ukraine with 11 years of experience as a Front-end developer and 1 year of experience as an UX/UI Designer.</p>
+          <p className="about__text">I’m an web designer and front-end developer from Vinnytsia, Ukraine with 11 years of experience as a Front-end developer and 1 year of experience as an UX/UI Designer!</p>
       </section>
     );
   }
